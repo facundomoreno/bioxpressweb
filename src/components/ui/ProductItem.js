4f02ed7d4467a0a431ed4e2efece33fcb10f23cf
Facundo Moreno <facundoivanmoreno1@gmail.com>
2020-07-14 10:41:22 -0300

    arreglos de diseño

--- src/components/ui/ProductItem.js
+++ src/components/ui/ProductItem.js
@@ -12,21 +12,9 @@ const ProductItem = ({ product }) => {
           <h2 className="title-product-item">{product.title}</h2>
           <ul>
             <li className="li-item-product">
-              <strong>Price: $ </strong> {product.price}
+              <strong>{"$" + product.price}</strong> 
             </li>
-            <li className="li-item-product">
-              <strong>Tienda: </strong> {product.store_name}
-            </li>
-            <li className="li-item-product">
-              <strong>Stock disponible: </strong> {product.stock}
-            </li>
-            <li className="li-item-product">
-              <strong>Descripción: </strong>
-              {product.ds_product}
-            </li>
-            <li className="li-item-product">
-              <strong>Categoría: </strong> {product.ds_category}
-            </li>
+           
           </ul>
         </div>
       </Link>
